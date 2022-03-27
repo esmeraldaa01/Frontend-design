be8e9f3c90f12dd793e4995c9b5b65cc706bea53
Esmeralda <lameesmeralda7@gmail.com>
2022-03-27 18:53:27 +0200

    d

--- src/assests/data.js
+++ src/assests/data.js
@@ -1,66 +1,31 @@
 const data = [
   {
-      title: 'It is Marys birthday',
-      category :'Featured',//
-      comment : 'happy birthday',
-      moreVideos :[
-          {
-              title : 'Title 1 about Featuring',
-              added : 'Added 2 hours ago'
-          },
-          {
-              title : 'Title 2 about Featuring',
-              added : 'Added 9 hours ago'
-          }
-      ]
-  },
-  {
-    title: 'It is Marys birthday',
-    category :'Featured',//
-    comment : 'happy birthday',
-    moreVideos :[
+    
+    title: "It is Marys birthday",
+    category: "Featured", //
+    comment: "happy birthday",
+    moreVideos: {
+      featured : [
         {
-            title : 'Title 1 about Featuring',
-            added : 'Added 2 hours ago'
+          title: "Title 11111 about Featuring",
+          added: "Added 2 hours ago",
         },
         {
-            title : 'Title 2 about Featuring',
-            added : 'Added 9 hours ago'
+          title: "Title 2 about Featuring",
+          added: "Added 9 hours ago",
         }
-    ]
-},
-  {
-      title: 'We are gonna make a trip',
-      category :'News',
-      comment : 'yes',
-      moreVideos :[
-          {
-              title : 'Title about News',
-              added : 'Added 1 hour ago'
-          }
-      ]
+      ],
+      more : [
+        {
+          title: "Title l about Featuring",
+          added: "Added l hours ago",
+        },
+        {
+          title: "Title r about Featuring",
+          added: "Added f hours ago",
+        }
+      ],
+    },
   },
-  {
-      title: 'I am hungry',
-      category :'Sport',//
-      comment : 'haha',
-      moreVideos :[
-          {
-              title : 'Title about Sport',
-              added : 'Added 6 hours ago'
-          }
-      ]
-  },
-   {
-       title: 'I am hungry',
-       category :'Enterteiment',
-       comment : 'haha',
-       moreVideos :[
-           {
-               title : 'Title about Enterteiment',
-               added : 'Added 4 hours ago'
-           }
-       ]
-   }
-]
+];
 export default data;
--- src/components/Container/RightSide/RightPart.js
+++ src/components/Container/RightSide/RightPart.js
@@ -1,42 +1,78 @@
 import React from "react";
-import {useContext} from "react";
+import { useContext } from "react";
 import featured from "../../../assests/featured.jpg";
-import  './RightPart.css';
-import {Context} from "../../Context/Context";
+import "./RightPart.css";
+import { Context } from "../../Context/Context";
+import data from "../../../assests/data";
 
 const RightPart = () => {
-    const {posts } = useContext(Context);
-    
-       const d = posts.map(x => x.category);
-const categories = d.filter((item, 
-    index) => d.indexOf(item) === index);
+  const { posts } = useContext(Context);
 
-console.log(categories , 'c')
-    return(
-        <div className='container'>
-            <h1 style={{fontWeight:800, fontSize:'20px'}}>Featured videos</h1>
-                          {categories.map(x => (
-            <div style={{marginTop:'150px'}}>
-            {posts.filter(post => post.category === x).map((p) => {
+  const d = posts.map((x) => x.category);
+  const categories = d.filter((item, index) => d.indexOf(item) === index);
+
+  return (
+    <div className="container">
+      <h1 style={{ fontWeight: 800, fontSize: "20px" }}>Featured videos</h1>
+      {categories.map((x) => (
+        <div style={{ marginTop: "150px" }}>
+          {posts
+            .filter((post) => post.category === x)
+            .map((p) => {
+              return (
+                <div id={x.toLowerCase()}>
+                  {p.moreVideos.featured.map((mvideo) => {
                     return (
-                        <div id={x.toLowerCase()}>
-                            {p.moreVideos.map((mvideo) => {
-                                return (
-                                    <div className='videos'>
-                                        <img width={200} height={100} src={featured}/>
-                                        <div style={{display:'flex', flexDirection:'column', margin:'5px'}}>
-                                        <p>{mvideo.title}</p>
-                                        <p>{mvideo.added}</p>
-                                        </div>
-                                    </div>
-                                )
-                            })}
+                      <div className="videos">
+                        <img width={200} height={100} src={featured} />
+                        <div
+                          style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            margin: "5px",
+                          }}
+                        >
+                          <p>{mvideo.title}</p>
+                          <p>{mvideo.added}</p>
                         </div>
-                    )
-                })} 
-            </div>
-              ))}
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
         </div>
-    )
-}
-export default RightPart
+      ))}
+       {categories.map((x) => (
+        <div style={{ marginTop: "150px" }}>
+          {posts
+            .filter((post) => post.category === x)
+            .map((p) => {
+              return (
+                <div id={x.toLowerCase()}>
+                  {p.moreVideos.more.map((mvideo) => {
+                    return (
+                      <div className="videos">
+                        <img width={200} height={100} src={featured} />
+                        <div
+                          style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            margin: "5px",
+                          }}
+                        >
+                          <p>{mvideo.title}</p>
+                          <p>{mvideo.added}</p>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
+        </div>
+      ))}
+    </div>
+  );
+};
+export default RightPart;
